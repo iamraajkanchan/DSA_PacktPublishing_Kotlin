4514ece1eecc8fcfa81b2d7f7e08a0d07fb5ca28
iamraajkanchan <iamraajkanchan@gmail.com>
2022-10-23 12:17:51 +0530

    UpperCasedList class is updated by Raj Kanchan 23.10.22

--- Chapter01/UpperCasedList.kt
+++ Chapter01/UpperCasedList.kt
@@ -1,17 +1,21 @@
 class UpperCasedList : ArrayList<String>() {
 
+    // Overriding add method to add a single element in ArrayList.
     override fun add(element: String): Boolean {
         return super.add(element.toUpperCase())
     }
 
+    // Overriding add method to add a single element with index in ArrayList.
     override fun add(index: Int, element: String) {
         super.add(index, element.toUpperCase())
     }
 
+    // Overriding addAll method to add a collection in ArrayList.
     override fun addAll(elements: Collection<String>): Boolean {
         return super.addAll(elements.map { it -> it.toUpperCase() })
     }
 
+    // Overriding addAll method to add a collection with index in ArrayList.
     override fun addAll(index: Int, elements: Collection<String>): Boolean {
         return super.addAll(index, elements.map { it -> it.toUpperCase() })
     }
@@ -26,9 +30,11 @@ class UpperCasedListImplementation {
         @JvmStatic
         fun main(args: Array<String>) {
             val names = UpperCasedList()
-            names.add("Chandra")
-            names.add("Rivu")
-            println("Names in upper case : $names")     // Output - [CHANDRA, RIVU]
+            names.add("raj kanchan")
+            names.add("taksh jaiswal")
+            val namesInLowerCase = arrayOf("vikrant singh", "pruthviraj", "gaurav mundse")
+            names.addAll(namesInLowerCase)
+            println("Names in upper case : $names")
         }
     }
 }
